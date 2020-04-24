19b18c6d9125208e8d91de96448ee56547409f4b
Mick de Graaf <mick@mickdegraaf.nl>
2020-04-25 00:08:51 +0200

    Initial commit

---
+++ blockchain/buidler.config.ts
@@ -0,0 +1,50 @@
+require("dotenv").config();
+import { BuidlerConfig, usePlugin, task } from "@nomiclabs/buidler/config";
+
+usePlugin("@nomiclabs/buidler-waffle");
+usePlugin("@nomiclabs/buidler-etherscan");
+usePlugin("buidler-typechain");
+usePlugin("solidity-coverage");
+
+const INFURA_API_KEY = process.env.INFURA_API_KEY || "";
+const KOVAN_PRIVATE_KEY = process.env.KOVAN_PRIVATE_KEY || "";
+const MAINNET_PRIVATE_KEY = process.env.MAINNET_PRIVATE_KEY || "";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
+
+interface ExtendedBuidlerConfig extends BuidlerConfig {
+  [x:string]: any
+}
+
+const config: ExtendedBuidlerConfig = {
+  defaultNetwork: "buidlerevm",
+  solc: {
+    version: "0.6.6",
+    
+  },
+  networks: {
+    kovan: {
+      url: `https://kovan.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [KOVAN_PRIVATE_KEY]
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [MAINNET_PRIVATE_KEY]
+    },
+    coverage: {
+      url: 'http://127.0.0.1:8555' // Coverage launches its own ganache-cli client
+    }
+  },
+  etherscan: {
+    // The url for the Etherscan API you want to use.
+    url: "https://api-rinkeby.etherscan.io/api",
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: ETHERSCAN_API_KEY
+  },
+  typechain: {
+    outDir: "typechain",
+    target: "ethers"
+  }
+};
+
+export default config;
