bbf44626abc556b9003bfc4c72680a4ce38f0975
Miles Minton <milesminton@gmail.com>
2020-05-02 14:35:24 -0700

    Completed Tabs Component

--- app/src/components/Tabs.js
+++ app/src/components/Tabs.js
@@ -1,37 +1,52 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Blockie, MetaMaskButton, Flex, Card, Box, Text, Loader } from 'rimble-ui';
-import isEmpty from 'lodash/isEmpty';
+import {Button} from 'rimble-ui';
+import styled from 'styled-components';
 
-// actions
-import { connectWalletAction } from '../actions/walletActions';
-import { getNetworkNameById, truncateHexString } from '../utils';
+const ComponentWrapper = styled.div`
+  width: 80vw;
+  display: flex;
+  flex-direction: Column;
+`;
+
+const TabsWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 20px;
+`;
 
 const Tabs = (props) => {
   const {tabData} = props;
-
   const [tabIndexSelected, setTabIndexSelected] = useState(0);
 
-  let tabs = [];
-  let content = <>;
+  console.log({tabData: tabData, tabIndexSelected: tabIndexSelected});
 
-  tabData.foreach((index, tabData) => {
+  let tabs = [];
+  let content = null;
+
+  let buttonStyle = {marginRight: "2px"};
+
+  tabData.forEach((tabData, index) => {
+    console.log({index: index});
     if (index === tabIndexSelected) {
-      tabs[] = <Button>{tabData.title}</Button>;
+      tabs.push(<Button style={buttonStyle}>{tabData.title}</Button>);
       content = tabData.content;
     } else {
-      tabs[] = <Button.Outline onClick={setTabIndexSelected.bind(index)}>{tabData.title}</Button.Outline>;
+      tabs.push(
+        <Button.Outline style={buttonStyle} onClick={() => setTabIndexSelected(index)}>
+          {tabData.title}
+        </Button.Outline>
+      );
     }
   });
 
   return (
-    <Flex flexDirection="column">
-      <Flex>
+    <ComponentWrapper>
+      <TabsWrapper>
         {tabs}
-      </Flex>
+      </TabsWrapper>
       {content}
-    </Flex>
+    </ComponentWrapper>
   );
 };
 
