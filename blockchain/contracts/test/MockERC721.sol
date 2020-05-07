eb6a04e3dfa343d987414f2e03a191d95d0455a8
Mick de Graaf <mick@mickdegraaf.nl>
2020-05-07 21:41:46 +0200

    Basic fracking test

---
+++ blockchain/contracts/test/MockERC721.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.6.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract MockERC721 is ERC721 {
+
+    uint256 nftCounter;
+
+    constructor (string memory _name, string memory _symbol) ERC721(_name, _symbol) public {
+
+    }
+
+    function mint(address _to, uint256 _amount) external {
+        _mint(_to, nftCounter);
+        nftCounter ++;
+    }
+}
