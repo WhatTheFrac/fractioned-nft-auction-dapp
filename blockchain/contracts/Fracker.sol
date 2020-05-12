e5ee8ce6519a8f694bf17cfec28b239d06a4936e
Mick de Graaf <mick@mickdegraaf.nl>
2020-05-12 23:40:36 +0200

    WIP fractionalize

--- blockchain/contracts/Fracker.sol
+++ blockchain/contracts/Fracker.sol
@@ -2,11 +2,15 @@ pragma solidity 0.6.4;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
-import "@pie-dao/smart-pools/contracts/smart-pools/PBasicSmartPool.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./aragon-minime/MiniMeToken.sol";
 import "./interfaces/IBFactory.sol";
 import "./interfaces/INFTReceiver.sol";
 import "./interfaces/ITokenReceiver.sol";
+import "./interfaces/IBPool.sol";
+
+
+//TODO consider making fracked token position an nft by itself 🤯
 
 contract Fracker {
     using Address for address;
@@ -19,9 +23,15 @@ contract Fracker {
         // address nftAddress;
         // uint256 nftId;
         // address nftReceiver;
-        address token;
+        address fracker; //address that fracked the token
+        uint256 frackTime; // timestamp when token was fracked
+        IERC20 token;
         IBPool balancerPool;
-        PBasicSmartPool smartPool;
+        uint256 balancerFlipDuration;
+        IERC20 auctionToken;
+        uint256 minAuctionBid;
+        uint256 minBidIncrease;
+        uint256 auctionDuration;
     }
 
     FrackedToken[] public frackedTokens;
@@ -35,23 +45,37 @@ contract Fracker {
         @dev Function is public because external gives stack too deep errors
     */
     function fractionalize(
-        // address _nftAddress,
-        // uint256 _nftId,
-        // address _nftReceiver,
-        // bytes memory _nftReceiverData,
+        address _nftAddress,
+        uint256 _nftId,
         string memory _tokenName,
         string memory _symbol,
         uint256 _initialSupply,
+        uint256 _amountToPool,
         address _poolToken, // e.g DAI
-        uint256 _targetPrice // estimated value of nft
+        uint256 _targetPrice, // estimated value of nft
+        uint256 _balancerFlipDuration, // How long it takes to flip the weight
+        uint256 _auctionToken,
+        uint256 _minAuctionBid,
+        uint256 _minBidIncrease,
+        uint256 _auctionDuration,
+
+        // Auction data
+        address lastBidder,
+        address lastBid,
+
+        bool settled
     ) public {
-        // IERC721 nftContract = IERC721(_nftAddress);
-        // Pull NFT
-        // nftContract.transferFrom(msg.sender, address(this), _nftId);
+        require(_amountToPool <= _initialSupply, "Cannot pool more than initialSupply");
+        require(_initialSupply != 0, "_initialSupply cannot be zero");
+        require(_auctionDuration >  _balancerFlipDuration, "Balancer needs to flip before the end of the auction");
+
+        IERC721 nftContract = IERC721(_nftAddress);
+        // Pull NFT, does not return anything according to ERC721 SPEC
+        nftContract.transferFrom(msg.sender, address(this), _nftId);
         // Allow NFT contract to pull token
         // nftContract.approve(_nftReceiver, _nftId);
 
-        // TODO use proxy to save gas
+        // TODO consider using proxy to save gas.
         // Create token
         MiniMeToken token = new MiniMeToken(
             address(0), // Not using clone token factory
@@ -65,11 +89,14 @@ contract Fracker {
 
         // Set fracked token data
         FrackedToken storage frackedTokenData = frackedTokens.push();
-        // frackedTokenData.nftAddress = _nftAddress;
-        // frackedTokenData.nftId = _nftId;
-        // frackedTokenData.nftReceiver = _nftReceiver;
-        // frackedTokenData.tokenReceiver = _tokenReceiver;
         frackedTokenData.token = address(token);
+        frackedTokenData.frackTime = block.timestamp;
+        frackedTokenData.balancerFlipDuration = _balancerFlipDuration;
+        frackedTokenData.fracker = msg.sender;
+        frackedTokenData.auctionToken = _auctionToken;
+        frackedTokenData.minAuctionBid = _minAuctionBid;
+        frackedTokenData.minBidIncrease = _minBidIncrease;
+        frackedTokenData.auctionDuration = _auctionDuration;
 
         // Mint tokens
         token.generateTokens(address(this), _initialSupply);
@@ -89,23 +116,46 @@ contract Fracker {
         poolToken.transferFrom(msg.sender, address(this), poolTokenAmount);
         poolToken.approve(address(balancerPool), poolTokenAmount);
         balancerPool.bind(address(poolToken), poolTokenAmount, BALANCER_2);
-
-
-        // TODO deploy smart pool through proxy to save gas
-        // Deploy smart pool
-        // PBasicSmartPool smartPool = new PBasicSmartPool();
-        // // Init smart pool
-        // // TODO better symbol and name
-        // smartPool.init(address(balancerPool), "WTF", "WTF", _initialSupply);
-        // // Send pool tokens to msg.sender
-        // smartPool.transfer(msg.sender, _initialSupply);
-        // // Enable trading
-        // smartPool.setPublicSwap(true);
-        // frackedTokenData.smartPool = smartPool;
+        balancerPool.setPublicSwap(true);
 
     }
 
 
+    // Pokes the balancer weights
+    function poke(uint256 _frackID) external {
+        FrackedToken storage frackedTokenData = frackedTokens[_frackID];
+
+
+        uint256 weightDifference = BALANCER_98 - BALANCER_2;
+        uint256 timePassed = block.timestamp - frackedTokenData.frackTime;
+        uint256 newWeightA = BALANCER_2 + weightDifference * timePassed / frackedTokenData.balancerFlipDuration;
+        uint256 newWeightB = BALANCER_100 - newWeightA;
+
+        IERC20 tokenA = frackedTokenData.token;
+        IERC20 tokenB = frackedTokenData.poolToken;
+
+        frackedTokenData.balancerPool.rebind(address(tokenA), frackedTokenData.getBalance(address(tokenA)), newWeightA);
+        frackedTokenData.balancerPool.rebind(address(tokenB), frackedTokenData.getBalance(address(tokenB)), newWeightB);
+    }
+
+    // Claim the nft after auction finished
+    function claimNFT(uint256 _frackID) external {
+
+    }
+
+    // Claim proceeds of user
+    function claimProceeds(uint256 _frackID, address _user) external {
+
+    }
+
+    // Remove liquidity from balancer pool, claim proceeds and send tokens back to user
+    function settle(uint256 _frackID) external {
+        
+    }
+
+    function bid(uint256 _frackID, uint256 _amount) external {
+
+    }
 
     // Boiler plate controller methods
     function proxyPayment(address) external payable returns(bool) {return true;}
