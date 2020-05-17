2de99c24c85371157c5a9924d79004658971fecb
Mick de Graaf <mick@mickdegraaf.nl>
2020-05-17 21:41:07 +0200

    WIP

--- blockchain/contracts/Fracker.sol
+++ blockchain/contracts/Fracker.sol
@@ -1,8 +1,11 @@
 pragma solidity 0.6.4;
+// pragma experimental ABIEncoderV2;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "@pie-dao/proxy/contracts/PProxy.sol";
 import "./aragon-minime/MiniMeToken.sol";
 import "./interfaces/IBFactory.sol";
 import "./interfaces/INFTReceiver.sol";
@@ -19,25 +22,27 @@ import "./interfaces/IBPool.sol";
 // TODO incentives for calling settle() and claimProceeds()
 // TODO ClaimProceeds
 
-contract Fracker {
+contract Fracker is ReentrancyGuard {
     using Address for address;
 
     uint256 public constant BALANCER_98 = 49 * 10 ** 18;
     uint256 public constant BALANCER_2 = 1 * 10 ** 18;
     uint256 public constant BALANCER_100 = 50 * 10 ** 18;
 
+    address tokenImplementation;
+
+    IERC20 public DAI;
+
     struct FrackedToken {
         IERC721 nftContract;
         uint256 nftId;
         address nftReceiver;
-        address fracker; //address that fracked the token
+        address fracker; //address that fracked the token set to 0x0000 after settlement
         uint256 frackTime; // timestamp when token was fracked
-        IERC20 token;
-        IERC20 poolToken;
+        MiniMeToken token;
         IBPool balancerPool;
         uint256 balancerFlipDuration;
-        IERC20 auctionToken;
-        // uint256 minAuctionBid;
+        uint256 minAuctionBid;
         uint256 minBidIncrease;
         uint256 auctionDuration;
         address lastBidder;
@@ -48,8 +53,10 @@ contract Fracker {
     FrackedToken[] public frackedTokens;
     IBFactory public balancerFactory;
 
-    constructor(address _balancerFactory) public {
+    constructor(address _balancerFactory, address _DAI) public {
         balancerFactory = IBFactory(_balancerFactory);
+        tokenImplementation = address(new MiniMeToken());
+        DAI = IERC20(_DAI);
     }
 
     /**
@@ -58,79 +65,96 @@ contract Fracker {
     function fractionalize(
         address _nftAddress,
         uint256 _nftId,
-        string memory _tokenName,
         string memory _symbol,
         uint256 _initialSupply,
         uint256 _amountToPool,
-        address _poolToken, // e.g DAI
+        // address _poolToken, // e.g DAI
         uint256 _targetPrice, // estimated value of nft
         uint256 _balancerFlipDuration, // How long it takes to flip the weight
-        uint256 _auctionToken,
-        // uint256 _minAuctionBid,
+        // uint256 _auctionToken,
+        uint256 _minAuctionBid,
         uint256 _minBidIncrease,
         uint256 _auctionDuration
-    ) public {
+    ) public nonReentrant {
         require(_amountToPool <= _initialSupply, "Cannot pool more than initialSupply");
         require(_initialSupply != 0, "_initialSupply cannot be zero");
         require(_auctionDuration >  _balancerFlipDuration, "Balancer needs to flip before the end of the auction");
 
-        IERC721 nftContract = IERC721(_nftAddress);
-        // Pull NFT, does not return anything according to ERC721 SPEC
-        nftContract.transferFrom(msg.sender, address(this), _nftId);
-        // Allow NFT contract to pull token
-        // nftContract.approve(_nftReceiver, _nftId);
+        {
+            IERC721 nftContract = IERC721(_nftAddress);
+            // Pull NFT, does not return anything according to ERC721 SPEC
+            nftContract.transferFrom(msg.sender, address(this), _nftId);
+        }
 
-        // TODO consider using proxy to save gas.
-        // Create token
-        MiniMeToken token = new MiniMeToken(
-            address(0), // Not using clone token factory
-            address(0), // Not a clone token
-            0, // No snapshot block
-            _tokenName,
-            18,
-            _symbol,
-            true
-        );
 
+        MiniMeToken token;
+        {
+            PProxy proxy = new PProxy();
+            proxy.setImplementation(tokenImplementation);
+            // Create token
+            token = MiniMeToken(address(proxy));
+        }
+
+        {
+            token.init(
+                address(0),
+                address(0),
+                0,
+                _symbol,
+                uint8(18),
+                _symbol,
+                true
+            );
+        }
         // Set fracked token data
         FrackedToken storage frackedTokenData = frackedTokens.push();
-        frackedTokenData.token = IERC20(address(token));
-        frackedTokenData.nftContract = IERC721(_nftAddress);
-        frackedTokenData.nftId = _nftId;
-        frackedTokenData.poolToken = IERC20(_poolToken);
-        frackedTokenData.frackTime = block.timestamp;
-        frackedTokenData.balancerFlipDuration = _balancerFlipDuration;
-        frackedTokenData.fracker = msg.sender;
-        frackedTokenData.auctionToken = IERC20(_auctionToken);
-        // frackedTokenData.minAuctionBid = _minAuctionBid;
-        frackedTokenData.minBidIncrease = _minBidIncrease;
-        frackedTokenData.auctionDuration = _auctionDuration;
+        frackedTokenData.token = token;
+
+        {
+            frackedTokenData.nftContract = IERC721(_nftAddress);
+            frackedTokenData.nftId = _nftId;
+            // frackedTokenData.poolToken = IERC20(_poolToken);
+            frackedTokenData.frackTime = block.timestamp;
+            frackedTokenData.balancerFlipDuration = _balancerFlipDuration;
+            frackedTokenData.fracker = msg.sender;
+            // frackedTokenData.auctionToken = IERC20(_auctionToken);
+            frackedTokenData.minAuctionBid = _minAuctionBid;
+            frackedTokenData.minBidIncrease = _minBidIncrease;
+            frackedTokenData.auctionDuration = _auctionDuration;
+        }
 
         // Mint tokens
         token.generateTokens(address(this), _initialSupply);
 
-        // Copy to memory to save gas on storage reads
-        IBPool balancerPool = IBPool(balancerFactory.newBPool());
-        frackedTokenData.balancerPool = balancerPool;
+        if(_amountToPool != 0) {
+            // Copy to memory to save gas on storage reads
+            IBPool balancerPool = IBPool(balancerFactory.newBPool());
+            frackedTokenData.balancerPool = balancerPool;
 
-        // // Bind fraction token
-        token.approve(address(balancerPool), _initialSupply);
-        balancerPool.bind(address(token), _initialSupply, BALANCER_98);
+            // TODO handle partial pooling
 
-        // Amount of pool token should be 2%
-        uint256 poolTokenAmount = _targetPrice * 2 / 100;
-        IERC20 poolToken = IERC20(_poolToken);
-        // Bind other token
-        poolToken.transferFrom(msg.sender, address(this), poolTokenAmount);
-        poolToken.approve(address(balancerPool), poolTokenAmount);
-        balancerPool.bind(address(poolToken), poolTokenAmount, BALANCER_2);
-        balancerPool.setPublicSwap(true);
+            // // // Bind fraction token
+            token.approve(address(balancerPool), _amountToPool);
+            balancerPool.bind(address(token), _amountToPool, BALANCER_98);
+
+            // // Amount of pool token should be 2%
+            uint256 poolTokenAmount = _targetPrice * _amountToPool / _initialSupply * 2 / 100;
+            // // Bind other token
+            DAI.transferFrom(msg.sender, address(this), poolTokenAmount);
+            DAI.approve(address(balancerPool), poolTokenAmount);
+            balancerPool.bind(address(DAI), poolTokenAmount, BALANCER_2);
+            balancerPool.setPublicSwap(true);
+        }
+
+        if(_amountToPool != _initialSupply) {
+            token.transfer(msg.sender, _initialSupply - _amountToPool);
+        }
+
 
     }
 
-
     // Pokes the balancer weights
-    function poke(uint256 _frackID) external {
+    function poke(uint256 _frackID) external nonReentrant {
         FrackedToken storage frackedTokenData = frackedTokens[_frackID];
 
 
@@ -139,67 +163,87 @@ contract Fracker {
         uint256 newWeightA = BALANCER_2 + weightDifference * timePassed / frackedTokenData.balancerFlipDuration;
         uint256 newWeightB = BALANCER_100 - newWeightA;
 
-        IERC20 tokenA = frackedTokenData.token;
-        IERC20 tokenB = frackedTokenData.poolToken;
+        MiniMeToken tokenA = frackedTokenData.token;
 
         frackedTokenData.balancerPool.rebind(address(tokenA), frackedTokenData.balancerPool.getBalance(address(tokenA)), newWeightA);
-        frackedTokenData.balancerPool.rebind(address(tokenB), frackedTokenData.balancerPool.getBalance(address(tokenB)), newWeightB);
+        frackedTokenData.balancerPool.rebind(address(DAI), frackedTokenData.balancerPool.getBalance(address(DAI)), newWeightB);
     }
 
 
     // Claim proceeds of user
-    function claimProceeds(uint256 _frackID, address _user) public {
+    function claimProceeds(uint256 _frackID, address _user) public nonReentrant {
+        _claimProceeds(_frackID, _user);
+    }
 
+    function _claimProceeds(uint256 _frackID, address _user) internal {
+        FrackedToken storage frackedTokenData = frackedTokens[_frackID];
+        require(frackedTokenData.fracker == address(0), "Must be settled");
+
+        MiniMeToken fracToken = frackedTokenData.token;
+
+        uint256 burnAmount = fracToken.balanceOf(_user);
+        // If user has 0 tokens return
+        if(burnAmount == 0) {
+            return;
+        }
+
+        uint256 payoutAmount = frackedTokenData.lastBid * burnAmount / fracToken.totalSupply();
+        frackedTokenData.lastBid = payoutAmount;
+
+        DAI.transfer(_user, payoutAmount);
+        fracToken.destroyTokens(_user, burnAmount);
     }
 
     // TODO catch revert on failure to send
     // Remove liquidity from balancer pool, claim proceeds and send tokens back to user, and send nft to buyer
-    function settle(uint256 _frackID) public {
+    function settle(uint256 _frackID) public nonReentrant {
         FrackedToken storage frackedTokenData = frackedTokens[_frackID];
         require(block.timestamp > frackedTokenData.frackTime + frackedTokenData.auctionDuration, "Auction not passed");
+        require(frackedTokenData.fracker != address(0), "Already settled");
 
-        IERC20 fracToken = frackedTokenData.token;
-        IERC20 poolToken = frackedTokenData.poolToken;
+        MiniMeToken fracToken = frackedTokenData.token;
 
         // Get balance before (reusing variables later)
         uint256 fracTokenAmount = fracToken.balanceOf(address(this));
-        uint256 poolTokenAmount = poolToken.balanceOf(address(this));
+        uint256 poolTokenAmount = DAI.balanceOf(address(this));
 
         // Unbind tokens in balancer pool
-        frackedTokenData.balancerPool.unbind(address(frackedTokenData.poolToken));
+        frackedTokenData.balancerPool.unbind(address(DAI));
         frackedTokenData.balancerPool.unbind(address(frackedTokenData.token));
 
         // Calc amount from balancer pool
         fracTokenAmount = fracToken.balanceOf(address(this)) - fracTokenAmount;
-        poolTokenAmount = poolToken.balanceOf(address(this)) - poolTokenAmount;
+        poolTokenAmount = DAI.balanceOf(address(this)) - poolTokenAmount;
 
         address fracker = frackedTokenData.fracker;
 
         // Send tokens to fracker
         fracToken.transfer(fracker, fracTokenAmount);
-        poolToken.transfer(fracker, poolTokenAmount);
+        DAI.transfer(fracker, poolTokenAmount);
 
+        // Set settled
+        frackedTokenData.fracker = address(0);
         // Claim proceeds of fracked token
-        claimProceeds(_frackID, fracker);
+        _claimProceeds(_frackID, fracker);
 
         // Send NFT to winner
         frackedTokenData.nftContract.transferFrom(address(this), frackedTokenData.lastBidder, frackedTokenData.nftId);
     }
 
     // TODO catch transfer failure
-    function bid(uint256 _frackID, uint256 _amount) external {
+    function bid(uint256 _frackID, uint256 _amount) external nonReentrant {
         FrackedToken storage frackedTokenData = frackedTokens[_frackID];
         require(block.timestamp < frackedTokenData.frackTime + frackedTokenData.auctionDuration, "Auction has passed");
-        // require(_amount > frackedTokenData.minAuctionBid, "Bid too low");
-        require(_amount > frackedTokenData.lastBid + frackedTokenData.minBidIncrease, "Bid increase too low");
+        require(_amount > frackedTokenData.minAuctionBid, "Bid too low");
+        require(_amount > frackedTokenData.lastBid + (frackedTokenData.lastBid * frackedTokenData.minBidIncrease / 1000), "Bid increase too low");
 
         if(frackedTokenData.lastBidder != address(0)) {
             // Send back previous bid to previous bidder
-            frackedTokenData.auctionToken.transfer(frackedTokenData.lastBidder, frackedTokenData.lastBid);
+            DAI.transfer(frackedTokenData.lastBidder, frackedTokenData.lastBid);
         }
 
         // Pull new bid
-        require(frackedTokenData.auctionToken.transferFrom(msg.sender, address(this), _amount), "Transfer failed");
+        require(DAI.transferFrom(msg.sender, address(this), _amount), "Transfer failed");
 
         frackedTokenData.lastBid = _amount;
         frackedTokenData.lastBidder = msg.sender;
--- blockchain/test/Fracker.ts
+++ blockchain/test/Fracker.ts
@@ -1,6 +1,6 @@
 import { ethers } from "@nomiclabs/buidler";
 import { Signer, Wallet, utils, constants } from "ethers";
-import { BigNumber } from "ethers/utils";
+import { BigNumber, parseEther, bigNumberify } from "ethers/utils";
 import chai from "chai";
 import { deployContract, solidity } from "ethereum-waffle";
 
@@ -37,21 +37,41 @@ describe("Fracker", function() {
         nft = await (new MockERC721Factory(signers[0])).deploy("WTF", "WTF");
         await nft.mint(account, 10);
         const balancerFactoryAddress = await deployBalancerFactory(signers[0]);
-        fracker = await deployContract(signers[0] as Wallet, FrackerArtifact, [balancerFactoryAddress], {gasLimit: 100000000}) as Fracker;
+        fracker = await deployContract(signers[0] as Wallet, FrackerArtifact, [balancerFactoryAddress, mockToken.address], {gasLimit: 100000000}) as Fracker;
         await nft.setApprovalForAll(fracker.address, true);
         await mockToken.approve(fracker.address, constants.MaxUint256);
     });
 
     describe("fractionalize", async() => {
-        it("Fractionalizing an NFT sending the token and NFT to an EOA should work", async() => {
-            const tx = await fracker.fractionalize("TEST", "TEST", constants.WeiPerEther, mockToken.address, constants.WeiPerEther.mul(100), {gasLimit: 100000000});
-            console.log((await tx.wait(1)).gasUsed.toString());
-            const frackedTokenData = await fracker.frackedTokens(0);
-            const token = IERC721Factory.connect(frackedTokenData.token, signers[0]);
-            const tokenBalance = await token.balanceOf(account2);
+        it("Should work", async() => {
+            const testName = "TESTNAME";
+            const testSymbol = "TESTSYMBOL";
+            const initialSupply = parseEther("1000000");
+            const oneDay = new BigNumber(60 * 60 * 24);
 
-            expect(tokenBalance).to.eq(constants.WeiPerEther);
-            // expect(nftOwner).to.eq(account2);
+            const tx = await fracker.fractionalize(
+                nft.address,
+                0,
+                testSymbol,
+                initialSupply,
+                parseEther("1"),
+                parseEther("1"),
+                oneDay,
+                parseEther("0.1"),
+                parseEther("0.01"),
+                oneDay.mul(2)
+            );
+            console.log((await tx.wait(1)).gasUsed.toString());
         });
+        // it("Fractionalizing an NFT sending the token and NFT to an EOA should work", async() => {
+        //     const tx = await fracker.fractionalize("TEST", "TEST", constants.WeiPerEther, mockToken.address, constants.WeiPerEther.mul(100), {gasLimit: 100000000});
+        //     console.log((await tx.wait(1)).gasUsed.toString());
+        //     const frackedTokenData = await fracker.frackedTokens(0);
+        //     const token = IERC721Factory.connect(frackedTokenData.token, signers[0]);
+        //     const tokenBalance = await token.balanceOf(account2);
+
+        //     expect(tokenBalance).to.eq(constants.WeiPerEther);
+        //     // expect(nftOwner).to.eq(account2);
+        // });
     });
 });
